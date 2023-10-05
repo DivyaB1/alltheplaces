23033d3d789b9f6f6310a10d4a1504150ddf7322
DivyaB1 <dbandapalle10@gmail.com>
2023-10-05 14:21:03 -0400

    adding category for peugeot_nl

--- locations/spiders/peugeot_nl.py
+++ locations/spiders/peugeot_nl.py
@@ -1,6 +1,7 @@
 import scrapy
 
 from locations.items import Feature
+from locations.categories import Categories, apply_category
 
 
 class PeugeotNLSpider(scrapy.Spider):
@@ -16,7 +17,7 @@ class PeugeotNLSpider(scrapy.Spider):
             address_details = store.get("address")
             coordinates = store.get("geolocation")
             contact_details = store.get("generalContact")
-            yield Feature(
+            item = Feature(
                 {
                     "ref": store.get("rrdi"),
                     "name": store.get("dealerName"),
@@ -31,3 +32,7 @@ class PeugeotNLSpider(scrapy.Spider):
                     "lon": float(coordinates.get("longitude")),
                 }
             )
+
+            apply_category(Categories.SHOP_CAR, item)
+            
+            yield item
