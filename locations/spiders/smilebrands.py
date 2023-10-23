a7ed603eea6dcbd410df70e9976d4da897a410c9
DivyaB1 <dbandapalle10@gmail.com>
2023-10-23 17:56:00 -0400

    adding category for smilebrands

--- locations/spiders/smilebrands.py
+++ locations/spiders/smilebrands.py
@@ -1,7 +1,14 @@
 from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
+from locations.categories import Categories, apply_category
 
 
 class SmilebrandsSpider(WPStoreLocatorSpider):
     name = "smilebrands"
     item_attributes = {"brand": "Smile Brands Inc.", "brand_wikidata": "Q108286823"}
     allowed_domains = ["smilebrands.com"]
+
+    def parse_item(self, item, location):
+        apply_category(Categories.DENTIST, item)
+        yield item
+
+
