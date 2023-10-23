90ae2a52ffd55d5651e9ae74b06fd2c075f2a301
DivyaB1 <dbandapalle10@gmail.com>
2023-10-23 18:08:49 -0400

    adding category to unicredit_de

--- locations/spiders/unicredit_de.py
+++ locations/spiders/unicredit_de.py
@@ -1,7 +1,12 @@
 from locations.storefinders.uberall import UberallSpider
+from locations.categories import Categories, apply_category
 
 
 class UnicreditDeSpider(UberallSpider):
     name = "unicredit_de"
     item_attributes = {"brand": "UniCredit", "brand_wikidata": "Q45568"}
     key = "QZ7auxGUWKL0MfAnUOgweafKIwrXPb"
+
+    def parse_item(self, item, location):
+        apply_category(Categories.BANK, item)
+        yield item
